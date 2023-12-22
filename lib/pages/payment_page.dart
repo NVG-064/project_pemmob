c1bd7da6250be5c63679eac855abada7a0fa15f4
Vernando <mayordeblo12@gmail.com>
2023-12-22 15:21:38 +0700

    revisi pembayaran

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -4,7 +4,6 @@ class PaymentPage extends StatelessWidget {
   final DateTime selectedDate;
   final int numberOfTickets;
 
-  // Constructor to receive selectedDate and numberOfTickets from the previous page
   const PaymentPage({
     Key? key,
     required this.selectedDate,
@@ -18,7 +17,6 @@ class PaymentPage extends StatelessWidget {
         title: Text('Payment'),
       ),
       body: Center(
-        // Wrap with Center widget
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -45,10 +43,8 @@ class PaymentPage extends StatelessWidget {
                 style: TextStyle(fontSize: 18),
               ),
               SizedBox(height: 20),
-              // Add your payment method options here (e.g., credit card, PayPal, etc.)
               ElevatedButton(
                 onPressed: () {
-                  // Implement payment processing logic
                   _handlePayment(context);
                 },
                 style: ElevatedButton.styleFrom(
@@ -65,8 +61,6 @@ class PaymentPage extends StatelessWidget {
   }
 
   void _handlePayment(BuildContext context) {
-    // Implement your payment processing logic here
-    // For example, you can navigate to a confirmation page after successful payment
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
@@ -83,7 +77,6 @@ class ConfirmationPage extends StatelessWidget {
   final DateTime selectedDate;
   final int numberOfTickets;
 
-  // Constructor to receive selectedDate and numberOfTickets from the payment page
   const ConfirmationPage({
     Key? key,
     required this.selectedDate,
@@ -97,7 +90,6 @@ class ConfirmationPage extends StatelessWidget {
         title: Text('Confirmation'),
       ),
       body: Center(
-        // Wrap with Center widget
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -119,13 +111,11 @@ class ConfirmationPage extends StatelessWidget {
                 style: TextStyle(fontSize: 18),
               ),
               SizedBox(height: 20),
-              // You can add more information or actions for the user
               Text(
                 'Destination : Museum Fatahillah',
                 style: TextStyle(fontSize: 18),
               ),
               SizedBox(height: 20),
-              // You can add more information or actions for the user
             ],
           ),
         ),
